b786e26c7b0e4b502192371861f1b666d9af5e9b
Ben Frenkel <ben@concussionjs.com>
2016-12-30 14:21:47 -0800

    updated to correct various small problems

--- tutorials/node/app.js
+++ tutorials/node/app.js
@@ -2,9 +2,12 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello Gigster!"')
+  res.send('Hello great, grand world of Gigster!!')
 })
 
-app.listen(80, function () {
-  console.log('Example app listening on port 80!')
+var server = app.listen(3000, function () {
+  var port = server.address().port;
+  console.log('Example app listening on port %s!', port)
 })
+
+module.exports = server;
