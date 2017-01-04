afa7e42b520afecaff0da7a6ee5bd7eb04535a53
Ben Frenkel <ben@concussionjs.com>
2017-01-04 05:44:50 -0800

    updated all files

--- tutorials/node/app.js
+++ tutorials/node/app.js
@@ -1,12 +1,24 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+
+/*--- SECTION BELOW IS FOR HEROKU DEPLOYMENT---
+*
+*  Heroku sets the environment variable PORT in the dyno
+*  your docker image is running on.
+*
+*  You need to run your service on PORT to forward requests
+*  externally over port 80. Services cannot be directly exposed
+*  over other ports on Heroku.
+*/
+
+// sets variable PORT to environment variable PORT, and if undefined to port 3000
+var port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
   res.send('Hello Gigster!!')
 })
 
-var server = app.listen(3000, function () {
-  var port = server.address().port;
+var server = app.listen(port, function () {
   console.log('Example app listening on port %s!', port)
 })
 
--- tutorials/node/deploy.sh
+++ tutorials/node/deploy.sh
@@ -1,15 +1,24 @@
 #!/usr/bin/env bash
 
+#--- WARNING: THIS FILE IS NOT CALLED BY THE DEFAULT CIRCLE.YML FIlE ---#
+#
 # This file pushes the current committed version of the
 # container to the gigster elastic container registry (ecr)
-# then deploys the file to your configured kubernetes cluster
+# then deploys the file to your configured kubernetes cluster 
 #
 # This file should be usable in its current form
+#
 #
 # Pre-reqs:
 #   The following environment variables must be set in your circleci’s projects settings
 #     + AWS_DEFAULT_REGION
-#     + AWS_ACCOUNT_ID  
+#     + AWS_ACCOUNT_ID
+#
+# --- Below environment variables are for kubernetes deployments ---#
+#     You'll need to set K8S_DEPLOY=TRUE in CircleCI project settings
+#     for the kubernetes deployments steps to execute.
+#
+#     + K8S_DEPLOY
 #     + K8S_CERTIFICATE_AUTHORITY_DATA
 #     + K8S_CLIENT_CERTIFICATE_DATA
 #     + K8S_CLIENT_KEY_DATA
@@ -31,15 +40,17 @@ JQ="jq --raw-output --exit-status"
 # creates the kubenconfig file required to enable circleci to update your kubernetes
 # deployment. It works by taking the environment variables set up in your circle.yml
 # file (described in step 5.7 of the Gigster standard deployment process guide) and
-# and merging them with the kubeconfig.yml.template
+# and merging them with the kubeconfig.yml.template.
+# only used if K8S_DEPLOY = "TRUE"
 k8s_config(){
     envsubst < k8s/kubeconfig.yml.template > k8s/kubeconfig.yml
 }
 
 # below command updates the image used by the kubernetes deployment to point to the 
-# most recent  build of your docker container
+# most recent  build of your docker container.
+# only used if K8S_DEPLOY = "TRUE"r
 k8s_deploy(){
-   kubectl set image deployment/$CIRCLE_PROJECT_REPONAME $CIRCLE_PROJECT_REPONAME=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$CIRCLE_PROJECT_REPONAME:$CIRCLE_BUILD_NUM
+   kubectl apply -f k8s/deployment.yml 
 }
 
 # Configures the AWS CLI
@@ -50,14 +61,18 @@ configure_aws_cli(){
 }
 
 # pushes the ECR image to the AWS elastic container registry (ECR)
+# only used if K8S_DEPLOY = "TRUE"
 push_ecr_image(){
-    # line below genereates the ecr login, and then uses eval to execute it
+    # line below generates the ecr login command, and then uses eval to execute it
     eval $(aws ecr get-login --region $AWS_DEFAULT_REGION)
     docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$CIRCLE_PROJECT_REPONAME:$CIRCLE_BUILD_NUM
 }
 
-# calls each method defined above in the correct sequence
 configure_aws_cli
 push_ecr_image
-k8s_config
-k8s_deploy
+
+# use below for kubernetes deployments
+if [ $K8S_DEPLOY == "TRUE" ]; then
+    k8s_config
+    k8s_deploy
+fi
