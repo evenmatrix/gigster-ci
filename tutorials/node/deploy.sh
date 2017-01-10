824fda79b6946b32a38d90de0a7d44d4cf9681ef
Ben Frenkel <ben@concussionjs.com>
2017-01-10 12:14:56 -0800

    updated to fix ecs deployment

--- tutorials/node/deploy.sh
+++ tutorials/node/deploy.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 
-#--- WARNING: THIS FILE IS NOT CALLED BY THE DEFAULT CIRCLE.YML FIlE ---#
-#
 # This file pushes the current committed version of the
 # container to the gigster elastic container registry (ecr)
-# then deploys the file to your configured kubernetes cluster 
+# then deploys the file to your configured kubernetes cluster
 #
 # This file should be usable in its current form
-#
 #
 # Pre-reqs:
 #   The following environment variables must be set in your circleci’s projects settings
@@ -44,13 +41,29 @@ JQ="jq --raw-output --exit-status"
 # only used if K8S_DEPLOY = "TRUE"
 k8s_config(){
     envsubst < k8s/kubeconfig.yml.template > k8s/kubeconfig.yml
+    envsubst < k8s/deployment.yml > k8s/deployment-merged.yml
 }
 
 # below command updates the image used by the kubernetes deployment to point to the 
 # most recent  build of your docker container.
 # only used if K8S_DEPLOY = "TRUE"r
 k8s_deploy(){
-   kubectl apply -f k8s/deployment.yml 
+   kubectl apply -f k8s/deployment-merged.yml 
+}
+
+heroku_deploy(){
+    [[ ! -s \"$(git rev-parse --git-dir)/shallow\" ]] || git fetch --unshallow
+    git push git@heroku.com:$HEROKU_APP_NAME.git $CIRCLE_SHA1:refs/heads/master
+}
+
+ecs_deploy(){
+    envsubst < docker-compose.yml.template > docker-compose.yml
+    export PATH=$PWD:$PATH
+    ecs-cli configure --region $AWS_DEFAULT_REGION --cluster $ECS_CLUSTER
+    ecs-cli compose \
+    --project-name $CIRCLE_USERNAME-$CIRCLE_PROJECT_REPONAME \
+    --file docker-compose.yml \
+    service create
 }
 
 # Configures the AWS CLI
@@ -65,7 +78,9 @@ configure_aws_cli(){
 push_ecr_image(){
     # line below generates the ecr login command, and then uses eval to execute it
     eval $(aws ecr get-login --region $AWS_DEFAULT_REGION)
+    docker tag $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$CIRCLE_PROJECT_REPONAME:$CIRCLE_BUILD_NUM $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$CIRCLE_PROJECT_REPONAME:latest
     docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$CIRCLE_PROJECT_REPONAME:$CIRCLE_BUILD_NUM
+    docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$CIRCLE_PROJECT_REPONAME:latest
 }
 
 configure_aws_cli
@@ -75,4 +90,12 @@ push_ecr_image
 if [ $K8S_DEPLOY == "TRUE" ]; then
     k8s_config
     k8s_deploy
+fi
+
+if [ $HEROKU_DEPLOY == "TRUE" ]; then
+    heroku_deploy
+fi
+
+if [ $ECS_DEPLOY == "TRUE" ]; then
+   ecs_deploy
 fi
